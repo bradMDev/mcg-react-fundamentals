fca44dcc33126bdc5637b33137d6feaa9198dd16
bradleymanning <bradmdev@gmail.com>
2018-05-30 18:09:35 -0500

    test commit

--- app/utils/api.js
+++ app/utils/api.js
@@ -38,6 +38,7 @@ module.exports = {
   battle: function(players) {
 
   },
+  
   fetchPopularRepos: function(language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
 
