f28b62045e95884782e62d161973d5c034a1fd17
bradleymanning <bradmdev@gmail.com>
2018-06-07 16:36:16 -0500

    add hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+// FOR PRODUCTION... adds:
+// NODE_ENV variable so React knows it has to build for production mode
+// and 
+// minifies code
+
+var config = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -21,4 +27,47 @@ module.exports = {
         template: 'app/index.html'
     })],
     mode: "development"
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      // Tells React we want to use the production version of React (Removes comments, etc.)
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV) // 'production'
+      }
+    }),
+    // Minifies all of our code
+    new webpack.optimize.UglifyJsPlugin()
+  );
 }
+
+module.exports = config;
+
+
+
+
+/* FOR DEVELOPMENT
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index_bundle.js',
+      publicPath: '/'
+  },
+  module: {
+      rules: [
+          { test: /\.(js)$/, use: 'babel-loader' },
+          { test: /\.(css)$/, use: [ 'style-loader', 'css-loader' ]}
+      ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: 'app/index.html'
+  })],
+  mode: "development"
+}
+*/
